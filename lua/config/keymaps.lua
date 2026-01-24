7fc27a235b37d8f4056e9bac89e8f25d17c2504f
Tylor Kolbeck <tylor.kolbeck@gmail.com>
2026-01-24 11:12:46 -0500

    compile and run current c++ file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,23 @@
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true })
 vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { noremap = true })
+
+-- Compile and run current C++ file from its directory
+vim.keymap.set("n", "<leader>r", function()
+  local file = vim.fn.expand("%:t") -- filename.cpp
+  local dir = vim.fn.expand("%:p:h") -- full path to directory
+  local output = vim.fn.expand("%:t:r") -- filename without extension
+
+  vim.cmd(
+    "split | terminal cd "
+      .. dir
+      .. " && g++ -std=c++20 -Wall -Wextra -g "
+      .. file
+      .. " -o "
+      .. output
+      .. " && ./"
+      .. output
+  )
+
+  vim.cmd("startinsert")
+end, { desc = "Compile & Run C++ (file dir)" })
